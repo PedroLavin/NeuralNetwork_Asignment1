27a346e4f74ad6397e97505822ac04774a1a3f6d
PedroLavin <63043948+PedroLavin@users.noreply.github.com>
2024-02-09 14:56:38 +0100

    Update Assignment_2_Gradient_Descent.py
    Add some comments

--- Assignment_2_Gradient_Descent.py
+++ Assignment_2_Gradient_Descent.py
@@ -50,7 +50,10 @@ plt.show()
 inputs = np.array((pd.read_csv("xi.csv", header = None)).transpose())
 labels = np.array(pd.read_csv("tau.csv", header = None))[0]
 
+len(inputs[0])
 
+def time_rate(learning_rate, time):
+  return 40*learning_rate / (40*learning_rate + time)
 
 def model(input, label, weights, epochs, learning_rate, test_input, test_label):
   E = []
@@ -60,23 +63,24 @@ def model(input, label, weights, epochs, learning_rate, test_input, test_label):
   for i in range(epochs):
 
 
-    # Select a random input and update the weights with the chosen input
+    # Iterate through all of the inputs and change the weights
 
+
+    # for j in range(len(input)):
     random_input = random.randint(0, len(input)-1)
 
       # change(weights, input[j], label[j], (time_rate(learning_rate, i)))
     change(weights, input[random_input], label[random_input], learning_rate)
-    # if i % int(epochs/10) == 0:
-
-    # Record the E and E_test
-    E.append(test(input, label, weights))
-    E_test.append(test(test_input, test_label, weights))
+    if i % int(epochs/100) == 0:
+      E.append(test(input, label, weights))
+      E_test.append(test(test_input, test_label, weights))
   return E, E_test
+
 def test(inputs, labels, weights):
   E = 0
   for i in range(len(inputs)):
-    E += 0.5 * (network_output(weights, inputs[i]) - labels[i])**2
-  return E / len(inputs)
+    E +=  0.5*(network_output(weights, inputs[i]) - labels[i])**2
+  return E / (len(inputs))
 
 # Initialize the weights
 
@@ -87,12 +91,44 @@ for i in range(2):
     column.append(random.random())
   initial_weights.append(column)
 
-learning_rate = 0.05
-E, E_test = model(inputs[0:500], labels[0:500], initial_weights, 2000, learning_rate, inputs[500:600], labels[500:600])
+learning_rate = 0.01
+train_batch = 2000
+test_batch = 1000
+t_max = 8000
+E, E_test = model(inputs[0:train_batch], labels[0:train_batch], initial_weights, t_max, learning_rate, inputs[train_batch:train_batch + test_batch], labels[train_batch:train_batch + test_batch])
 
-plt.plot(E, label = "Training Error")
-plt.plot(E_test, label = "Testing Error")
-plt.xlabel("Epochs")
+initial_weights
+
+plt.plot(np.arange(0, len(E)*t_max/100, t_max/100),E, label = "Training Error")
+plt.plot(np.arange(0, len(E)*t_max/100, t_max/100),E_test, label = "Testing Error")
+plt.xlabel("Time")
 plt.ylabel("Error (E)")
+# plt.title("Learning Rate: " + str(learning_rate))
 plt.legend()
 plt.show()
+
+test(inputs[401:403], labels[401:403], initial_weights)
+
+inputs[2100], labels[2100]
+network_output(initial_weights, inputs[2100])
+
+# plt.plot([x for x in labels[150:200]])
+plt.plot([x for x in labels[425:450]])
+plt.plot(l)
+# plt.plot([-x for x in initial_weights[0]])
+# plt.plot(initial_weights[1])
+
+test(inputs[425:450], labels[425:450], initial_weights)
+erro = []
+for i in range(425, 450, 1):
+  erro.append(0.5*(network_output(initial_weights, inputs[i]) - labels[i])**2)
+
+
+
+erro
+
+l = []
+for i in range(425, 450,1):
+  l.append(network_output(initial_weights, inputs[i]))
+
+initi
